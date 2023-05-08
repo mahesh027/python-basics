7d2786d230bc357b0e7f3321cadde2c93c7cf012
K Mahesh <mahesh0352002@gmail.com>
2023-05-08 16:12:54 +0530

    initial commit

---
+++ math.py
@@ -0,0 +1,14 @@
+import math
+print(math.pi)
+print(math.e) #can use math.e or math.pi instead of typing the constant 3.14something or 2.71something
+print(math.fabs(-5)) #prints absolute value
+print(math.ceil(1.5555))
+print(math.floor(1.5555))
+print(math.sqrt(5))
+print(math.pow(2,3)) #to find 2^3
+print(math.log2(2))
+print(math.log10(2))
+print(math.tan(math.pi/4)) #tan pi/4
+
+#to find factorial using math function
+print(math.factorial(6))
